122a4ad56bf3e7f129c60c00cef83e67668fd58c
maria <maria@DESKTOP-V4PMJTL>
2022-02-14 14:43:18 -0500

    Completed Assignment

--- VariablePractice/Form1.cs
+++ VariablePractice/Form1.cs
@@ -19,8 +19,46 @@ namespace VariablePractice
 
         private void hockeyButton_Click(object sender, EventArgs e)
         {
-
+            string playerName = "Wayne Gretzky";
+            int playerNumber = 99;
+            outputLabel.Text = $"{playerName} is number {playerNumber}";
         }
 
+        private void payButton_Click(object sender, EventArgs e)
+        {
+            double hours = 25;
+            double pay = 18.75;
+            double earned = hours * pay;
+            outputLabel.Text = $"{hours} hours at ${pay} per hour equals ${earned}";
+        }
+
+        private void circleButton_Click(object sender, EventArgs e)
+        {
+            double radius = 15;
+            double pi = 3.14;
+            double area = pi * radius * radius;
+            outputLabel.Text = $"The area of circle with a radius of {radius}cm is {area}cm^2";
+        }
+
+        private void carpetButton_Click(object sender, EventArgs e)
+        {
+            double length = 8.5;
+            double width = 6;
+            double costPerMeter = 19.95;
+            double area = length * width;
+            double totalCost = costPerMeter * area;
+            outputLabel.Text = $"The area of a room with dimensions {length}m x {width}m is {area}m^2\n\nThe cost to carpet this area at {costPerMeter} per square metre is ${totalCost}";
+        }
+
+        private void billButton_Click(object sender, EventArgs e)
+        {
+            double shirtCost = 12.49;
+            double moneyGiven = 20;
+            double taxRate = 0.13;
+            double taxAmount = shirtCost * taxRate;
+            double totalBill = shirtCost + taxAmount;
+            double change = moneyGiven - totalBill;
+            outputLabel.Text = $"Bill of Sale\n\nShirt:      {shirtCost}\n\nTax:        {taxAmount}\nTotal:      {totalBill}\n\nTendered:   {moneyGiven}\nChange:     {change}";
+        }
     }
 }
